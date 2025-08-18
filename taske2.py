f1ba7d7b822d7ec019e5e818dcae496de7aba23c
Muhammar933 <muhammar4567@gmail.com>
2025-08-18 16:13:32 +0500

    Updated book search route and fixed path conflict

---
+++ taske2.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+#User profile management application
+
+class UserProfile(BaseModel):
+    id: int
+    name: str
+    email: str
+
+# Fake database
+database = []
+
+@app.post("/profiles")
+def create_user(profile: UserProfile):
+    database.append(profile.dict())
+    return {"message":"User profile created successfully", "profile": profile}
+
+@app.get("/profiles")
+def get_profiles():
+    if not database:
+        raise HTTPException(status_code=404, detail="No profiles fount")
+    return {"profiles": database}
+
+@app.get("/profiles/{profile_id}")
+def get_id_profile(profile_id: int):
+    for profile in database:
+        if profile["id"] == profile_id:
+            return {"profile": profile}
+    raise HTTPException(status_code=404, detail="Profile not found")
+
+@app.put("/profiles/{profile_id}")
+def update_profile(profile_id: int, profile: UserProfile):
+    for i, existing_profile in enumerate(database):
+        if existing_profile['id'] == profile_id:
+            database[i] = profile.dict()
+            return {"message": "Profile updated successfully", "profile": profile}
+    raise HTTPException(status_code=404, detail="Profile not found")
